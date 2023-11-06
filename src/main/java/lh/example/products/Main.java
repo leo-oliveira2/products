a8b24a424245f75327d0cb68fda5766192dadb61
Luca Temes D'Andrea <lucatemex@gmail.com>
2023-11-06 18:07:49 -0300

    tests

---
+++ src/main/java/lh/example/products/Main.java
@@ -0,0 +1,10 @@
+package lh.example.products;
+
+import java.beans.Transient;
+
+public class Main {
+   
+    
+}
+
+
--- src/test/java/lh/example/products/ProductsApplicationTests.java
+++ src/test/java/lh/example/products/ProductsApplicationTests.java
@@ -2,12 +2,23 @@ package lh.example.products;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ProductsApplicationTests {
+	ImpostoCalculator imposto;
+	DescontoManager desconto;
+
 
 	@Test
-	void contextLoads() {
+	void calcularImpostoTest(){
+		assertEquals(imposto.calcularImposto(10), 1);
+		assertEquals(imposto.calcularImposto(100),10);
 	}
 
+	@Test
+	void aplicarDescontoTest(){
+		assertEquals(desconto.aplicarDesconto(100, 10), 90);
+	} 
+	
 }
