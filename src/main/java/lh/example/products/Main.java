11935afb6a0962e7041aefaa02f24fb50003a4fc
Luca Temes D'Andrea <lucatemex@gmail.com>
2023-11-20 18:18:40 -0300

    data

--- src/main/java/lh/example/products/Main.java
+++ src/main/java/lh/example/products/Main.java
@@ -40,5 +40,16 @@ public class Main {
         }
 
         return produtos;
+
+         /*
+         * Quando um cliente quer comprar um ou mais itens ele faz um pedido. No pedido consta o identificador do pedido, o nome do cliente, 
+         * e uma lista de itens. Cada item tem o código do produto, e a quantidade desejada. A partir do pedido é gerado um orçamento. 
+         * Um orçamento possui um código, a data, o nome do cliente, o pedido, o somatório dos custos dos itens do pedido, o valor do imposto (10%), 
+         * o valor do desconto (o padrão é 5% para pedidos com mais de 5 itens) e o valor final para o consumidor. Todos os orçamentos são armazenados
+         * para consulta futura. Se o cliente aprovar o orçamento basta indicar que deseja efetivar a venda informando o número do orçamento. 
+         * Neste momento o sistema deve verificar a viabilidade da venda, isto é, 
+         * se existem produtos suficientes no estoque para atender ao pedido. Em caso positivo, o sistema faz a baixa no estoque e marca o
+         */
+
     }
 }
--- src/main/java/lh/example/products/Pedido.java
+++ src/main/java/lh/example/products/Pedido.java
@@ -1,24 +1,44 @@
 package lh.example.products;
 
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.List;
+
+import org.springframework.cglib.core.Local;
 
 public class Pedido {
     private int idPedido;
     private List<Produto> produtos;
     private int valorTotal;
+    private LocalDateTime dataDoPedido;
+    private LocalDateTime dataDeExpiracao;
     private Calendar data;
     private boolean efetivado;
+    private LocalDateTime agora;
 
     public Pedido(int idPedido, List<Produto> produtos, Calendar data, boolean efetivado) {
         this.idPedido = idPedido;
         this.produtos = produtos;
-        this.data = data;
+        this.data = data; //temp
+        this.dataDoPedido = dataDoPedido.now();
+        this.dataDeExpiracao = dataDoPedido.plusDays(21);
         this.efetivado = efetivado;
         this.valorTotal = calculaValorTotal(produtos);
     }
 
     public Pedido(int i, int j, int k, int l, int m, boolean b) {
+    }
+
+    public boolean verificar(){
+        agora = agora.now();
+        if(dataDeExpiracao.isBefore(agora)){
+            //setar status como cancelado
+            return false;
+        }else{
+            return true;
+        }
+
+        
     }
 
     public void setEfetivado(){
@@ -39,12 +59,17 @@ public class Pedido {
         return valorTotal;
     }
 
-    public Calendar getData() {
-        return data;
+
+    public LocalDateTime getDataExpiracao() {
+        return dataDeExpiracao;
     }
 
     public boolean isEfetivado() {
         return efetivado;
+    }
+
+    public Calendar getData(){
+        return data;
     }
 
     public static int calculaValorTotal(List<Produto> produtos) {
